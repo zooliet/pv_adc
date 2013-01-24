44436af95953916c9c6dfe8214b1f03a69f44cca
Junhyun Shin <hl1sqi@gmail.com>
2013-01-24 15:06:32 +0900

    deleted module folder

--- ext/pv_adc.c
+++ ext/pv_adc.c
@@ -3,29 +3,25 @@
 #include <string.h>
 #include <stdlib.h>
 
-static VALUE read_fpga(int argc, VALUE *argv, VALUE serlf)
+static VALUE read_fpga(int argc, VALUE *argv, VALUE self)
 {
 	VALUE val, result;
 	FILE *fp;
 	unsigned short buf[1024];
-	int i;
-	
-	memset((char *)buf, 0, 2048);  // zero stuffing...
+	unsigned int i;
 	
 	if((fp = fopen("/dev/pvdspb_adc1", "rb")) == NULL) {
 		printf("File open faild. You may check /dev/pvdspb_adc1 and pvdspb_adc.ko.\n");
 	}
 	else {
-		fread(buf, 2, 1024, fp);			// when fixed later, it should be 256 -> 1024
+	 	fread(buf, 2, 1024, fp);			// when fixed later, it should be 256 -> 1024
 		fclose(fp);
 	}
-	
-	result = rb_ary_new();
-	for(i = 0; i < 1024; i++) {		
-    // printf("%d: 0x%04X", i, buf[i]);		
-		rb_ary_push(result, INT2FIX(buf[i]));
-	}
 
+	result = rb_ary_new();
+        for(i=0; i < 1024; i++) {
+		rb_ary_push(result, INT2FIX(buf[i]));
+        }
 	return result;
 }	
 
